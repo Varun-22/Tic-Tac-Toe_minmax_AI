a25eb105323c3f6aea888db6de92fe1ef51be1c2
Varun <varun.cce16@sot.pdpu.ac.in>
2020-08-27 02:32:07 +0530

    added medium level

--- game.py
+++ game.py
@@ -64,3 +64,45 @@ def makemat():
       mat[i].append('_')
       mov.append([i+1,j+1])
   return mat,mov
+
+# medium level game
+def checkmed(m,x,val):
+  for i in x:
+    temp = []
+    temp.append(3-i[1])
+    temp.append(i[0]-1)
+    #check rc, for win
+    if(temp[0]!=temp[1]):
+      r1 = (m[(temp[0]+1)%3][(temp[1])%3] == m[(temp[0]+2)%3][(temp[1])%3]) and (m[(temp[0]+2)%3][(temp[1])%3]==val)
+      r2 = (m[(temp[0])%3][(temp[1]+1)%3] == m[(temp[0])%3][(temp[1]+2)%3]) and (m[(temp[0])%3][(temp[1]+2)%3]==val)
+      raw = r1 or r2
+      if(raw):
+        return [x.index(i),'win']
+    #check rcd, for win
+    elif(temp[0]==temp[1]):
+      r1 = (m[(temp[0]+1)%3][(temp[1]+1)%3] == m[(temp[0]+2)%3][(temp[1]+2)%3]) and (m[(temp[0]+2)%3][(temp[1]+2)%3]==val)
+      r2 = (m[(temp[0]+1)%3][(temp[1])%3] == m[(temp[0]+2)%3][(temp[1])%3]) and (m[(temp[0]+2)%3][(temp[1])%3]==val)
+      r3 = (m[(temp[0])%3][(temp[1]+1)%3] == m[(temp[0])%3][(temp[1]+2)%3]) and (m[(temp[0])%3][(temp[1]+2)%3]==val)
+      raw = r1 or r2 or r3
+      if(raw):
+        return [x.index(i),'win']
+  for i in x:
+    temp = []
+    temp.append(3-i[1])
+    temp.append(i[0]-1)
+    #check rc, for block
+    if(temp[0]!=temp[1]):
+      r1 = (m[(temp[0]+1)%3][(temp[1])%3] == m[(temp[0]+2)%3][(temp[1])%3]) and (m[(temp[0]+2)%3][(temp[1])%3]!=val) and (m[(temp[0]+2)%3][(temp[1])%3]!='_')
+      r2 = (m[(temp[0])%3][(temp[1]+1)%3] == m[(temp[0])%3][(temp[1]+2)%3]) and (m[(temp[0])%3][(temp[1]+2)%3]!=val) and (m[(temp[0])%3][(temp[1]+2)%3]!='_')
+      raw = r1 or r2
+      if(raw):
+        return [x.index(i),'block']
+    #check rcd, for block
+    elif(temp[0]==temp[1]):
+      r1 = (m[(temp[0]+1)%3][(temp[1]+1)%3] == m[(temp[0]+2)%3][(temp[1]+2)%3]) and (m[(temp[0]+2)%3][(temp[1]+2)%3]!=val) and (m[(temp[0]+2)%3][(temp[1]+2)%3]!='_')
+      r2 = (m[(temp[0]+1)%3][(temp[1])%3] == m[(temp[0]+2)%3][(temp[1])%3]) and (m[(temp[0]+2)%3][(temp[1])%3]!=val) and (m[(temp[0]+2)%3][(temp[1])%3]!='_')
+      r3 = (m[(temp[0])%3][(temp[1]+1)%3] == m[(temp[0])%3][(temp[1]+2)%3]) and (m[(temp[0])%3][(temp[1]+2)%3]!=val) and (m[(temp[0])%3][(temp[1]+2)%3]!='_')
+      raw = r1 or r2 or r3
+      if(raw):
+        return [x.index(i),'block']    
+  return [random.randint(0,len(x)-1),'random']
